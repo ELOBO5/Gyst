6a1377f3801aa76784ccf1cd88faaca88642da55
“ELOBO5” <elicialobo@hotmail.com>
2021-08-30 15:32:22 +0100

    add verifyToken function to routes

--- api/Routes/habits.js
+++ api/Routes/habits.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const habitsController = require('../controllers/habits.js');
+const habitsController = require("../controllers/habits.js");
 
-router.get('/', habitsController.index);
-router.get('/:id', habitsController.show);
-router.post('/', habitsController.create);
-router.delete('/:id', habitsController.destroy);
+const { verifyToken } = require("../middleware/auth");
+
+router.get("/", verifyToken, habitsController.index);
+router.get("/:id", verifyToken, habitsController.show);
+router.post("/", verifyToken, habitsController.create);
+router.delete("/:id", verifyToken, habitsController.destroy);
 
 module.exports = router;
