8e5dc8c04b29f040e848986a243bdd32d6dc43ae
Tobi Kuyoro <tobikuyo@gmail.com>
2021-08-30 16:53:33 +0100

    Function to remove habit from client

--- client/static/js/dash.js
+++ client/static/js/dash.js
@@ -1,11 +1,15 @@
 const BASE_URL = 'http://localhost:3000/habits';
 
 const getAllHabits = async () => {
-	const response = await fetch(BASE_URL);
-	const { habits } = await response.json();
+	try {
+		const response = await fetch(BASE_URL);
+		const { habits } = await response.json();
 
-	for (const habit of habits) {
-		addHabitToDocument(habit, habit.frequency);
+		for (const habit of habits) {
+			addHabitToDocument(habit, habit.frequency);
+		}
+	} catch (error) {
+		console.log('Error getting all habits from server ', error);
 	}
 };
 
@@ -23,16 +27,33 @@ const addHabitToDocument = (habit, frequency) => {
  * @param {object} habit should contain `habit`, `frequency`, `has_priority`, `habit_count` and `habit_streak` keys.
  */
 const updateHabit = async (id, habit) => {
-	const options = {
-		method: 'PUT',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify(habit)
-	};
+	try {
+		const options = {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(habit)
+		};
 
-	const response = await fetch(`${BASE_URL}/${id}`, options);
-	const data = await response.json();
+		const response = await fetch(`${BASE_URL}/${id}`, options);
+		const data = await response.json();
 
-	// TODO: - Use the 'data' to adjust what's on the page, once HTML has been completed and designed.
+		// TODO: - Use the 'data' to adjust what's on the page, once HTML has been completed and designed.
+	} catch (error) {
+		console.log('Error updating habit in client ', error);
+	}
+};
+
+const deleteHabit = async id => {
+	try {
+		const options = {
+			method: 'DELETE',
+			headers: { 'Content-Type': 'application/json' }
+		};
+
+		await fetch(`${BASE_URL}/${id}`, options);
+	} catch (error) {
+		console.log('Error deleting habit in client ', error);
+	}
 };
 
 getAllHabits();
