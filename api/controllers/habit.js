4c25d9f07fccd5ea7522628cb6e7f0f4884e9a57
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-08-30 10:45:42 +0100

    Created show function

--- api/controllers/habit.js
+++ api/controllers/habit.js
@@ -5,7 +5,17 @@ async function index(req, res) {
         const habits = await Habit.all;
         res.status(200).json({habits});
     } catch (err) {
-        console.log('Could not get habits');
+        console.error('Could not get habits');
         res.status(500).json({err});
+    }
+}
+
+async function show(req, res) {
+    try {
+        const habit = await Habit.findByHabitId(req.params.id);
+        res.status(200).json(habit);
+    } catch (err) {
+        console.error('Could not find habit');
+        res.status(404).json({err});
     }
 }
