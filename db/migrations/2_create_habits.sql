7f7c84cedf6ed55e61a3def92ae5d4ffb3ccb190
“ELOBO5” <elicialobo@hotmail.com>
2021-08-30 09:59:16 +0100

    create habit schema

---
+++ db/migrations/2_create_habits.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE habits (
+    id serial PRIMARY KEY,
+    habit VARCHAR(50) NOT NULL,
+    frequency VARCHAR (50) NOT NULL,
+    has_priority BOOLEAN NOT NULL,
+    created_at DATE NOT NULL,
+    habit_count INT NOT NULL,
+    habit_streak INT NOT NULL,
+    user_id INT NOT NULL
+);
