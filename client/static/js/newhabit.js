f1ad13632549189b087fe86df2ab1734fd7487c8
teganGowing <tgowing97@gmail.com>
2021-08-30 15:04:33 +0100

    creating new habit posting to dashboard

--- client/static/js/newhabit.js
+++ client/static/js/newhabit.js
@@ -1,4 +1,55 @@
-// Create habit
+//Save habit
 
 
-//disable priority
+// Create habit on dashboard
+
+const postHabit = event => {
+    event.preventDefault();
+    const habitEntry = document.querySelector('#habit-entry').value;
+    fetch('https://localhost:3000/habits', {
+        method: 'POST',
+        body: JSON.stringify({
+            message: habitEntry,
+        }),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error, 'Error posting entry'));
+};
+
+
+
+function createPost(data) {
+    const indvHabitContainer = document.createElement('ul');
+    const habit = document.createElement('li');
+    const buttonplaceholder = document.createElement('li')
+    const markAsDone = document.createElement('button')
+    const streakCounter = document.createElement('li')
+
+    indvHabitContainer.setAttribute('class', 'habitContainer');
+    habit.setAttribute('class', 'habitListItem habit');
+    buttonplaceholder.setAttribute('class', 'habitListItem buttonplaceholder')
+    markAsDone.setAttribute('class', 'markAsDoneButton')
+    streakCounter.setAttribute('class', 'habitListItem streakCounter')
+
+    habit.textContent = data.habitEntry;
+    entries.style.outline = 'transparent';
+
+    indvHabitContainer.appendChild(habit);
+    indvHabitContainer.appendChild(buttonplaceholder);
+    buttonplaceholder.appendChild(markAsDone);
+    indvHabitContainer.appendChild(streakCounter);
+
+    //use a switch statement for the different frequencies? 
+    habitListContainer = document.querySelector('#dailyContainer')
+    habitListContainer.appendChild(indvHabitContainer);
+
+}
+
+
+
+//disable priority if > 3
