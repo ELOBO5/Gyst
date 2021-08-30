64c534b9963395935cd97d3ddaf784310507a3ea
“ELOBO5” <elicialobo@hotmail.com>
2021-08-30 10:07:08 +0100

    add config

---
+++ api/dbConfig/dev_seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (username, email, password) 
+VALUES
+('ellie', 'ellie87@gmail.co.uk', '325efwrg342'),
+('jeff', 'jeffgold@gmail.com', 'kljb3257hbs'),
+('han', 'hannah8872@gmail.com', 'kuiru124');
+
+INSERT INTO habits (habit, frequency, has_priority, created_at, habit_count, habit_streak, user_id) 
+VALUES
+('sleep', 'daily', true, '2021-08-13', 3, 10, 1),
+('exercise', 'weekly', true, '2021-07-29', 5, 3, 2),
+('exercise', 'daily', true, '2021-08-20', 3, 1, 3),
+('sleep', 'daily', true, '2021-08-05', 3, 3, 1);
