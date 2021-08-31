52c458f9da1814e7bf02e0cadd584833f82720b2
teganGowing <tgowing97@gmail.com>
2021-08-31 15:31:51 +0100

    script tag

--- api/Routes/habits.js
+++ api/Routes/habits.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const { verifyToken } = require("../middleware/auth");
 const habitsController = require("../controllers/habits.js");
 
-router.get("/", verifyToken, habitsController.index);
-router.get("/:id", verifyToken, habitsController.show);
-router.post("/", verifyToken, habitsController.create);
-router.put("/:id/info", verifyToken, habitsController.updateInfo);
-router.put("/:id/completed", verifyToken, habitsController.toggleCompleted);
-router.put("/:id/reset", verifyToken, habitsController.dailyReset);
-router.delete("/:id", verifyToken, habitsController.destroy);
+router.get("/", habitsController.index);
+router.get("/:id", habitsController.show);
+router.post("/", habitsController.create);
+router.put("/:id/info", habitsController.updateInfo);
+router.put("/:id/completed", habitsController.toggleCompleted);
+router.put("/:id/reset", habitsController.dailyReset);
+router.delete("/:id", habitsController.destroy);
 
 module.exports = router;
