17be2746c052b415b1e7ce13223d1574e5486614
teganGowing <tgowing97@gmail.com>
2021-08-31 10:11:53 +0100

    individual habit containers

--- client/static/js/newhabit.js
+++ client/static/js/newhabit.js
@@ -34,11 +34,12 @@ function createPost(data) {
     indvHabitContainer.setAttribute('class', 'habitContainer');
     habit.setAttribute('class', 'habitListItem habit');
     buttonplaceholder.setAttribute('class', 'habitListItem buttonplaceholder')
-    markAsDone.setAttribute('class', 'markAsDoneButton')
+    markAsDone.setAttributes('class', 'markAsDoneButton's)
     streakCounter.setAttribute('class', 'habitListItem streakCounter')
 
     habit.textContent = data.habitEntry;
-    entries.style.outline = 'transparent';
+    button.value = 
+    indvHabitContainer.style.outline = 'transparent';
 
     indvHabitContainer.appendChild(habit);
     indvHabitContainer.appendChild(buttonplaceholder);
