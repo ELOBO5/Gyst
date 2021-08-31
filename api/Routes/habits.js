0c28942be2d8ea29813dabfdc91f1523ff6884cb
Tobi Kuyoro <tobikuyo@gmail.com>
2021-08-31 08:45:45 +0100

    Create controllers and routes for update methods

--- api/Routes/habits.js
+++ api/Routes/habits.js
@@ -5,7 +5,9 @@ const habitsController = require('../controllers/habits.js');
 router.get('/', verifyToken, habitsController.index);
 router.get('/:id', verifyToken, habitsController.show);
 router.post('/', verifyToken, habitsController.create);
-router.put('/:id', verifyToken, habitsController.update);
+router.put('/:id/info', verifyToken, habitsController.updateInfo);
+router.put('/:id/completed', verifyToken, habitsController.toggleCompleted);
+router.put('/:id/reset', verifyToken, habitsController.dailyReset);
 router.delete('/:id', verifyToken, habitsController.destroy);
 
 module.exports = router;
