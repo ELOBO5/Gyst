b36aed58633c0dd6231ca3991b9484a49302f479
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-01 15:16:46 +0100

    Added tests for success and failure of static findByHabitId method

--- api/tests/unit/models/Habit.test.js
+++ api/tests/unit/models/Habit.test.js
@@ -29,4 +29,35 @@ describe('Habit', () => {
             }
         })
     })
+
+    describe('static findByHabitId', () => {
+        test('returns habit with particular id on successful db query', async () => {
+            const habitData = {id: 35, habit: 'Sleep', frequency: 'monthly', has_priority: true, created_at: '2020-02-24', user_id: 3};
+            const id = habitData.id;
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [habitData] });
+            
+            const habit = await Habit.findByHabitId(id);
+
+            expect(habit).toBeInstanceOf(Habit);
+            expect(habit.id).toEqual(35);
+            expect(habit.habit).toEqual('Sleep');
+            expect(habit.frequency).toEqual('monthly');
+            expect(habit.has_priority).toEqual(true);
+            expect(habit.created_at).toEqual('2020-02-24');
+            expect(habit.user_id).toEqual(3);
+        })
+
+        test('returns error notifying failure of retrieval on unsuccessful db query', async () => {
+            const habitData = {id: 35, habit: 'Sleep', frequency: 'monthly', has_priority: true, created_at: '2020-02-24', user_id: 3};
+            const id = habitData.id;
+
+            try {
+                jest.spyOn(db, 'query').mockRejectedValueOnce(Error());
+                await Habit.findByHabitId(id);
+            } catch (err) {
+                expect(err).toEqual('Habit not found');
+            }
+        })
+    })
 })
