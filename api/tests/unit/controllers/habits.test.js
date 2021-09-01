778a191a7db02bf0f9a1bfb1830843b47c805e3e
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-01 17:49:44 +0100

    Added test for success of index controller

--- api/tests/unit/controllers/habits.test.js
+++ api/tests/unit/controllers/habits.test.js
@@ -5,3 +5,21 @@ const mockSend = jest.fn();
 const mockJson = jest.fn();
 const mockStatus = jest.fn(() => ({send: mockSend, json: mockJson}));
 const mockRes = {status: mockStatus};
+
+describe('Habits Controller', () => {
+    beforeEach(() => jest.clearAllMocks())
+
+    afterAll(() => jest.resetAllMocks())
+
+    describe('index', () => {
+        test('returns all habits with a 200 status code', async () => {
+            jest.spyOn(Habit, 'all', 'get')
+                .mockResolvedValueOnce(['habit1', 'habit2']);
+
+            await habitsController.index(null, mockRes);
+
+            expect(mockStatus).toHaveBeenCalledWith(200);
+            expect(mockJson).toHaveBeenCalledWith(['habit1', 'habit2']);
+        })
+    })
+})
