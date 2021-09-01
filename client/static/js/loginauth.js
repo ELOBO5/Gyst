a57879ef15af39179656a850cdb39ecb11e37ab1
“ELOBO5” <elicialobo@hotmail.com>
2021-09-01 10:45:26 +0100

    create loginauth file

---
+++ client/static/js/loginauth.js
@@ -0,0 +1,42 @@
+async function requestLogin(e) {
+  e.preventDefault();
+  try {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: e.target.email.value,
+        password: e.target.password.value
+      })
+    };
+    const r = await fetch(
+      `https://get-your-sht-together.herokuapp.com/auth/login`,
+      options
+    );
+    const data = await r.json();
+    console.log("data", data);
+    if (!data.success) {
+      throw new Error("Login not authorised");
+    }
+    login(data.token);
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+function login(token) {
+  const user = jwt_decode(token);
+  localStorage.setItem("token", token);
+  localStorage.setItem("username", user.username);
+  localStorage.setItem("email", user.email);
+  localStorage.setItem("id", user.id);
+  window.location.href = "http://127.0.0.1:5503/client/dash.html";
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "http://127.0.0.1:5503/client/index.html";
+}
+
+const loginForm = document.querySelector("form#login");
+loginForm.addEventListener("submit", requestLogin);
