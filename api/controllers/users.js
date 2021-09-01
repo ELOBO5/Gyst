39e5c58f802c06b2de2de48a79b5a96c49d5a5bf
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-01 11:23:45 +0100

    Added some basic controllers

---
+++ api/controllers/users.js
@@ -0,0 +1,44 @@
+const User = require('../models/user.js');
+
+async function index(req, res) {
+    try {
+        const users = await User.all;
+        res.status(200).json(users);
+    } catch (err) {
+        console.error('Could not get Users');
+        res.status(500).json({err});
+    }
+}
+
+async function show(req, res) {
+    try {
+        const user = await User.findByEmail(req.params.email);
+        res.status(200).json(user);
+    } catch (err) {
+        console.error('Could not find User');
+        res.status(404).json({err});
+    }
+}
+
+async function create(req, res) {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json(newUser);
+    } catch (err) {
+        console.error('Could not create User');
+        res.status(422).json({err});
+    }
+}
+
+async function destroy(req, res) {
+    try {
+        const User = await User.findByEmail(req.params.id);
+        await User.destroy();
+        res.status(204).end();
+    } catch (err) {
+        console.error('Could not delete User');
+        res.status(404).json({err});
+    }
+}
+
+module.exports = { index, show, create, destroy };
