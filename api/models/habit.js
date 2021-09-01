cbe341e11f7a7076c0d8bdadb64f4bf45862a1d5
“ELOBO5” <elicialobo@hotmail.com>
2021-09-01 15:43:01 +0100

    update create model for habits

--- api/models/habit.js
+++ api/models/habit.js
@@ -7,8 +7,8 @@ class Habit {
     this.frequency = data.frequency;
     this.has_priority = data.has_priority;
     this.created_at = data.created_at;
-		this.habit_count = data.habit_count || 0;
-		this.habit_streak = data.habit_streak || 0;
+    this.habit_count = data.habit_count || 0;
+    this.habit_streak = data.habit_streak || 0;
     this.completed = data.completed;
     this.completed_counter = data.completed_counter || 0;
     this.user_id = data.user_id;
@@ -30,7 +30,7 @@ class Habit {
     return new Promise(async (resolve, reject) => {
       try {
         let habitData = await db.query(`SELECT * FROM habits WHERE id = $1;`, [
-          id,
+          id
         ]);
         let habit = new Habit(habitData.rows[0]);
         resolve(habit);
@@ -66,11 +66,11 @@ class Habit {
             habitData.frequency,
             habitData.has_priority,
             created_at,
-            habitData.user_id,
+            habitData.user_id
           ]
         );
-
-        resolve(result.rows[0]);
+        let habit = new Habit(result.rows[0]);
+        resolve(habit);
       } catch (err) {
         reject("Habit could not be created");
       }
@@ -78,37 +78,37 @@ class Habit {
   }
 
   updateInfo(body) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const { habit, frequency, has_priority } = body;
-				const data = await db.query(
-					`UPDATE habits SET habit = $1, frequency = $2, has_priority = $3 WHERE id = $4 RETURNING *;`,
-					[habit, frequency, has_priority, this.id]
-				);
-				const updatedHabit = new Habit(data.rows[0]);
-				resolve(updatedHabit);
-			} catch (error) {
-				reject('Habit could not be updated');
-			}
-		});
-	}
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { habit, frequency, has_priority } = body;
+        const data = await db.query(
+          `UPDATE habits SET habit = $1, frequency = $2, has_priority = $3 WHERE id = $4 RETURNING *;`,
+          [habit, frequency, has_priority, this.id]
+        );
+        const updatedHabit = new Habit(data.rows[0]);
+        resolve(updatedHabit);
+      } catch (error) {
+        reject("Habit could not be updated");
+      }
+    });
+  }
 
   toggleCompleted(body) {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const { completed, habit_streak, completed_counter } = body;
-				const data = await db.query(
-					`UPDATE habits SET completed = $1, habit_streak = $2, completed_counter = $4 WHERE id = $3 RETURNING *;`,
-					[completed, habit_streak, this.id, completed_counter]
-				);
-				const updatedHabit = new Habit(data.rows[0]);
-				resolve(updatedHabit);
-			} catch (error) {
-				reject('Habit completed could not be toggled');
-			}
-		});
-	}
-  
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { completed, habit_streak, completed_counter } = body;
+        const data = await db.query(
+          `UPDATE habits SET completed = $1, habit_streak = $2, completed_counter = $4 WHERE id = $3 RETURNING *;`,
+          [completed, habit_streak, this.id, completed_counter]
+        );
+        const updatedHabit = new Habit(data.rows[0]);
+        resolve(updatedHabit);
+      } catch (error) {
+        reject("Habit completed could not be toggled");
+      }
+    });
+  }
+
   dailyReset(body) {
     return new Promise(async (resolve, reject) => {
       try {
@@ -119,10 +119,9 @@ class Habit {
         const updatedHabit = new Habit(data.rows[0]);
         resolve(updatedHabit);
       } catch (error) {
-        reject('Habit could not be reset');
+        reject("Habit could not be reset");
       }
     });
-
   }
 
   destroy() {
