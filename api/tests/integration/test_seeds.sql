5f7b41b7f53777713839637e7d2706144745d98b
Gorazd Sumkovski <gorazd.sumkovski@gmail.com>
2021-09-02 15:42:34 +0100

    Added completed column

--- api/tests/integration/test_seeds.sql
+++ api/tests/integration/test_seeds.sql
@@ -5,10 +5,10 @@ VALUES
 ('Test User 1', 'test1@email.com', 'pass'),
 ('Test User 2', 'test2@email.com', 'word');
 
-INSERT INTO habits (habit, frequency, has_priority, habit_count, habit_streak, completed_counter, user_id)
+INSERT INTO habits (habit, frequency, has_priority, habit_count, habit_streak, completed, completed_counter, user_id)
 VALUES
-('1 first habit', 'daily', TRUE, 5, 2, 0, 1),
-('1 second habit', 'weekly', FALSE, 7, 0, 0, 1),
-('1 third habit', 'monthly', TRUE, 6, 0, 0, 1),
-('2 first habit', 'daily', FALSE, 60, 15, 0, 2),
-('2 second habit', 'monthly', TRUE, 5, 5, 0, 2);
+('1 first habit', 'daily', TRUE, 5, 2, FALSE, 0, 1),
+('1 second habit', 'weekly', FALSE, 7, 0, FALSE, 0, 1),
+('1 third habit', 'monthly', TRUE, 6, 0, FALSE, 0, 1),
+('2 first habit', 'daily', FALSE, 60, 15, FALSE, 0, 2),
+('2 second habit', 'monthly', TRUE, 5, 5, FALSE, 0, 2);
